1aca757b61c1dd18c79ade6ed71c2a44dc98f331
Sean Ransonette <sransonette@gmail.com>
2020-07-10 21:50:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = [1, 2, 3]
+  add_element_to_end_of_array.push(4)
 end
 
 def add_element_to_start_of_array(array, element)
